664793281a1055296e76614b1ca7eba57781f92c
mhviraf <hosseinali.sut@gmail.com>
2020-03-11 16:22:43 -0600

    indentation error resolved

--- Tokenize.py
+++ Tokenize.py
@@ -6,8 +6,8 @@ class tokenize(object):
     
     def __init__(self, lang, savetokens):
         self.nlp = spacy.load(lang)
-		self.lang = lang
-		self.savetokens = savetokens
+	self.lang = lang
+	self.savetokens = savetokens
             
     def tokenizer(self, sentence):
         sentence = re.sub(
@@ -18,8 +18,8 @@ class tokenize(object):
         sentence = re.sub(r"\?+", "?", sentence)
         sentence = sentence.lower()
         tokens = [tok.text for tok in self.nlp.tokenizer(sentence) if tok.text != " "]
-		if self.savetokens == 1:
-			np.save(self.lang + '_sentences.npy', sentence)
-			np.save(self.lang + '_tokens.npy', sentence)
+	if self.savetokens == 1:
+		np.save(self.lang + '_sentences.npy', sentence)
+		np.save(self.lang + '_tokens.npy', sentence)
         
         return tokens
