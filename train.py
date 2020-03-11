9e4acbfe4c0084d223d279d2ff99dc6e8ea84a60
Keven Carlson <u0823391@notch204.int.chpc.utah.edu>
2020-03-11 11:37:01 -0600

    fixed gpu in train.py

--- train.py
+++ train.py
@@ -28,8 +28,8 @@ def train_model(model, opt):
                     
         for i, batch in enumerate(opt.train): 
 
-            src = batch.src.transpose(0,1)
-            trg = batch.trg.transpose(0,1)
+            src = batch.src.transpose(0,1).to('cuda')
+            trg = batch.trg.transpose(0,1).to('cuda')
             trg_input = trg[:, :-1]
             src_mask, trg_mask = create_masks(src, trg_input, opt)
             preds = model(src, trg_input, src_mask, trg_mask)
