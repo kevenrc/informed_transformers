7856ac7df68b54842953488ecf6047371cf1ad82
Massoud Hosseinali <32608959+LiquPy@users.noreply.github.com>
2020-03-11 19:19:41 -0600

    Update Tokenize.py

--- Tokenize.py
+++ Tokenize.py
@@ -6,8 +6,6 @@ class tokenize(object):
 
     def __init__(self, lang, savetokens):
         self.nlp = spacy.load(lang)
-        self.lang = lang
-        self.savetokens = savetokens
 
     def tokenizer(self, sentence):
         sentence = re.sub(
@@ -17,9 +15,5 @@ class tokenize(object):
         sentence = re.sub(r"\,+", ",", sentence)
         sentence = re.sub(r"\?+", "?", sentence)
         sentence = sentence.lower()
-        tokens = [tok.text for tok in self.nlp.tokenizer(sentence) if tok.text != " "]
-        if self.savetokens == 1:
-            np.save(self.lang + '_sentences.npy', sentence)
-            np.save(self.lang + '_tokens.npy', sentence)
 
-        return tokens
+        return [tok.text for tok in self.nlp.tokenizer(sentence) if tok.text != " "]
