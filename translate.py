84042f3b9272a1f29273418b9bd2e9e35a427821
mhviraf <hosseinali.sut@gmail.com>
2020-04-17 12:47:49 -0600

    fixed savetokens flag issue

--- translate.py
+++ translate.py
@@ -67,12 +67,13 @@ def main():
     parser.add_argument('-max_len', type=int, default=80)
     parser.add_argument('-d_model', type=int, default=512)
     parser.add_argument('-n_layers', type=int, default=6)
-    parser.add_argument('-src_lang', required=True)
-    parser.add_argument('-trg_lang', required=True)
+    parser.add_argument('-src_lang', default='en')
+    parser.add_argument('-trg_lang', default='fr')
     parser.add_argument('-heads', type=int, default=8)
     parser.add_argument('-dropout', type=int, default=0.1)
     parser.add_argument('-no_cuda', action='store_true')
     parser.add_argument('-floyd', action='store_true')
+    parser.add_argument('-savetokens', type=int, default=0)
     
     opt = parser.parse_args()
 
