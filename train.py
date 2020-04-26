20410be74e3a65d45391010b0c1f66499dbff940
Massoud Hosseinali <hosseinali.sut@gmail.com>
2020-04-25 18:40:45 -0600

    commented out translation dictionary conversion to avoid overwritting the processed dict

--- train.py
+++ train.py
@@ -99,13 +99,13 @@ def main():
 
     opt.train = create_dataset(opt, SRC, TRG)
 
-    # convert translation dictionary to tokens ditionary
-    translation_dictionar = pickle.load(open('data/translation_dictionary.p', 'rb'))
-    new_dict = {}
-    for en_word, fr_word in translation_dictionar.items():
-        new_dict[SRC.vocab.stoi[en_word]] = TRG.vocab.stoi[fr_word.lower()]
+    # # convert translation dictionary to tokens ditionary
+    # translation_dictionar = pickle.load(open('data/translation_dictionary.p', 'rb'))
+    # new_dict = {}
+    # for en_word, fr_word in translation_dictionar.items():
+    #     new_dict[SRC.vocab.stoi[en_word]] = TRG.vocab.stoi[fr_word.lower()]
     
-    pickle.dump(new_dict, open('data/tokenized_translation_dictionary.p', 'wb'))
+    # pickle.dump(new_dict, open('data/tokenized_translation_dictionary.p', 'wb'))
 
     model = get_model(opt, len(SRC.vocab), len(TRG.vocab))
     model = model.to(device=opt.device)
