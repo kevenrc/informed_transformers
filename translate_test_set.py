9a6cc734408f3584f8b14a49fb994419f5028ccf
Massoud Hosseinali <hosseinali.sut@gmail.com>
2020-04-27 21:30:39 -0600

    translate test set in batches

---
+++ translate_test_set.py
@@ -0,0 +1,98 @@
+import argparse
+import time
+import torch
+from Models import get_model
+from Process import *
+import torch.nn.functional as F
+from Optim import CosineWithRestarts
+from Batch import create_masks
+import pdb
+import dill as pickle
+import argparse
+from Models import get_model
+from Beam import beam_search
+from nltk.corpus import wordnet
+from torch.autograd import Variable
+import re
+
+def get_synonym(word, SRC):
+    syns = wordnet.synsets(word)
+    for s in syns:
+        for l in s.lemmas():
+            if SRC.vocab.stoi[l.name()] != 0:
+                return SRC.vocab.stoi[l.name()]
+            
+    return 0
+
+def multiple_replace(dict, text):
+  # Create a regular expression  from the dictionary keys
+  regex = re.compile("(%s)" % "|".join(map(re.escape, dict.keys())))
+
+  # For each match, look-up corresponding value in dictionary
+  return regex.sub(lambda mo: dict[mo.string[mo.start():mo.end()]], text) 
+
+def translate_sentence(sentence, model, opt, SRC, TRG):
+    
+    model.eval()
+    indexed = []
+    sentence = SRC.preprocess(sentence)
+    for tok in sentence:
+        if SRC.vocab.stoi[tok] != 0 or opt.floyd == True:
+            indexed.append(SRC.vocab.stoi[tok])
+        else:
+            indexed.append(get_synonym(tok, SRC))
+    sentence = Variable(torch.LongTensor([indexed]))
+    if opt.device == 0:
+        sentence = sentence.cuda()
+    
+    sentence = beam_search(sentence, model, SRC, TRG, opt)
+
+    return  multiple_replace({' ?' : '?',' !':'!',' .':'.','\' ':'\'',' ,':','}, sentence)
+
+def translate(opt, model, SRC, TRG):
+    sentences = opt.text.lower().split('.')
+    translated = []
+
+    for sentence in sentences:
+        translated.append(translate_sentence(sentence + '.', model, opt, SRC, TRG).capitalize())
+
+    return (' '.join(translated))
+
+
+def main():
+    
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-load_weights', required=True)
+    parser.add_argument('-k', type=int, default=3)
+    parser.add_argument('-max_len', type=int, default=80)
+    parser.add_argument('-d_model', type=int, default=512)
+    parser.add_argument('-n_layers', type=int, default=6)
+    parser.add_argument('-src_lang', default='en')
+    parser.add_argument('-trg_lang', default='fr')
+    parser.add_argument('-heads', type=int, default=8)
+    parser.add_argument('-dropout', type=int, default=0.1)
+    parser.add_argument('-no_cuda', action='store_true')
+    parser.add_argument('-floyd', action='store_true')
+    parser.add_argument('-savetokens', type=int, default=0)
+    
+    opt = parser.parse_args()
+
+    opt.device = 0 if opt.no_cuda is False else -1
+ 
+    assert opt.k > 0
+    assert opt.max_len > 10
+
+    SRC, TRG = create_fields(opt)
+    model = get_model(opt, len(SRC.vocab), len(TRG.vocab))
+    
+    test_set = open('data/en_test').read().strip().split('\n')
+    translations = []
+    for en_sentence in test_set:
+        opt.text = en_sentence
+        phrase = translate(opt, model, SRC, TRG)
+        translations.append(phrase)
+
+    open('data/fr_test_translations', 'w').write('\n'.join(translations))
+
+if __name__ == '__main__':
+    main()
